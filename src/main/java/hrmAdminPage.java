fa07c8212fad6bc2cc4ebeb33b154d79255030bf
baozhen1113 <ngoc.ntnb22840@sinhvien.hoasen.edu.vn>
2026-05-09 12:54:11 +0700

    20 locator hrmpage

--- src/main/java/hrmAdminPage.java
+++ src/main/java/hrmAdminPage.java
@@ -34,20 +34,70 @@ public class hrmAdminPage {
         btnLogin.click();
 
         // locator01
-        By byMenuAdmin =  By.xpath("//a[contains(normalize-space(),'Admin')]");
+        By byMenuAdmin = By.xpath("//a[contains(normalize-space(),'Admin')]");
         WebElement menuAdmin = wait.until(ExpectedConditions.visibilityOfElementLocated(byMenuAdmin));
+        menuAdmin.click();
 
         //locator02
-        By byMenuPIM =  By.xpath("//a[contains(normalize-space(),'PIM')]");
+        By byMenuPIM = By.xpath("//a[contains(normalize-space(),'PIM')]");
 
         //locator03
-        By byMenuLeave =  By.xpath("//a[contains(normalize-space(),'Leave')]");
+        By byMenuLeave = By.xpath("//a[contains(normalize-space(),'Leave')]");
 
         //locator04
         By byTxtSearchUsername = By.xpath("//label[text()='Username']/ancestor::div[contains(@class,'oxd-input-group')]//input");
 
         //locator05
-        
+        By bySelUserRole = By.xpath("//label[text()='User Role']/following::div[contains(@class,'oxd-select-text')][1]");
+
+        //locator06
+        By byAdminOption = By.xpath("//div[@role='option']//span[text()='Admin");
+
+        //locator07
+        By byESSOption = By.xpath("//div[@role='option']//span[text()='ESS");
+
+        //locator08
+        By byTxtEmployeeName = By.xpath("//label[text()='Employee Name']/ancestor::div[contains(@class,'oxd-input-group')]//input");
+
+        //locator09
+        By byBtnReset = By.xpath("//button[text()=' Reset ']");
+
+        //locator10
+        By byBtnSearch = By.xpath("//button[text()=' Search ']");
+
+        //locator11
+        By bySelUserManagement = By.xpath("//li[@class='oxd-topbar-body-nav-tab --parent --visited']");
+
+        //locator12
+        By bySecondRowUsername = By.xpath("(//div[contains(@class,'oxd-table-row--with-border')])[3]//div[@role='cell'][2]");
+
+        //locator13
+        By bySecondRowUserRole = By.xpath("(//div[contains(@class,'oxd-table-row--with-border')])[3]//div[@role='cell'][3]");
+
+        //locator14
+        By bySecondRowDeleteBtn = By.xpath("(//div[contains(@class,'oxd-table-row--with-border')])[3]//i[contains(@class,'bi-trash')]");
+
+        //locator15
+        By bySecondRowEditBtn = By.xpath("(//div[contains(@class,'oxd-table-row--with-border')])[3]//i[contains(@class,'bi-pencil-fill')]");
+
+        //locator16
+        By byUsernameHeader = By.xpath("//div[@role='columnheader' and contains(.,'Username')]");
+
+        //locator17
+        By byUserRoleHeader = By.xpath("//div[@role='columnheader' and contains(.,'User Role')]");
+
+        //locator18
+        By byBreadcrumb = By.xpath("//span[contains(@class,'oxd-topbar-header-breadcrumb')]");
+
+        //locator19
+        By ByBtnCollapseMenu = By.xpath("//button[.//i[contains(@class,'bi-chevron-left')]]");
+
+        //locator20
+        By byBtnCaretUp = By.xpath("//button[.//i[contains(@class,'bi-caret-up-fill')]]");
+
+        //locator21
+        By byBtnAdd = By.xpath("//button[text()=' Add ']");
+
         chromeDriver.quit();
     }
 }
